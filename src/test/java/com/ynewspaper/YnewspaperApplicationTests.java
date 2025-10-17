ea94bdeae821a97d6ebd4b94956a418c9336d6dc
Abaraira <cibersteficodes@gmail.com>
2025-10-17 12:28:35 +0200

    initial commit - project skeleton

---
+++ src/test/java/com/ynewspaper/YnewspaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynewspaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnewspaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
