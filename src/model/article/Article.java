c5d00c24250f1abd847c1d85c79fc2f4c178700f
Montc027 <lymomuca@gmail.com>
2025-10-20 13:05:13 +0200

    Created article file

--- src/model/article/Article.java
+++ src/model/article/Article.java
@@ -1,3 +1,21 @@
+package model.article;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+import lombok.Data;
+@Data
+@Entity
+@Table(name = "articles")
+
 public class Article {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    private String date;
+    private String category;
+    private String user;
+
 }
