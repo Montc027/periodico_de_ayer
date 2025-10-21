723ed729e4ae8e3126ea0f66ebd87a5530dea31f
Abaraira <cibersteficodes@gmail.com>
2025-10-21 15:51:12 +0200

    Task: C-POST CreateArticle- ArticleController.java

--- src/main/java/com/ynewspaper/controller/ArticleController.java
+++ src/main/java/com/ynewspaper/controller/ArticleController.java
@@ -1,5 +1,26 @@
 package com.ynewspaper.controller;
 
+import com.ynewspaper.dto.ArticleDTO;
+import com.ynewspaper.entity.Article;
+import com.ynewspaper.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/articles")
+
 public class ArticleController {
-    
+    private final ArticleService articleService;
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
 }
+
+    @PostMapping
+    public Article createArticle(@RequestBody ArticleDTO dto) {
+        return articleService.createArticle(dto);
+       
+    }
+}
+
