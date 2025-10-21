8bf58c45501caee3de4733d32b1eec19d6eff14e
Abaraira <cibersteficodes@gmail.com>
2025-10-21 13:46:51 +0200

    Task: C-POST CreateArticle- User.java

--- src/main/java/com/ynewspaper/entity/User.java
+++ src/main/java/com/ynewspaper/entity/User.java
@@ -1,5 +1,22 @@
 package com.ynewspaper.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Article> articles;
     
 }
